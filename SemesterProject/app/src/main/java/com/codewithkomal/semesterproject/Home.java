d646542dc30a8699b1579c4342476ff281cb9d6e
Nimra Rani <mcsf19m040@pucit.edu.pk>
2021-06-01 06:46:04 -0700

    Changes By Nimra

--- SemesterProject/app/src/main/java/com/codewithkomal/semesterproject/Home.java
+++ SemesterProject/app/src/main/java/com/codewithkomal/semesterproject/Home.java
@@ -1,12 +1,11 @@
 package com.codewithkomal.semesterproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Home extends AppCompatActivity {
 
@@ -19,18 +18,18 @@ public class Home extends AppCompatActivity {
         Category1 = findViewById(R.id.categ01);
         Category2 = findViewById(R.id.categ02);
 
-        Category1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Home.this,Category1.class);
-                startActivity(intent);
-            }
-        });
-
         Category2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Home.this,Category2.class);
+                startActivity(intent);
+            }
+        });
+
+        Category1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Home.this,Category1.class);
                 startActivity(intent);
             }
         });
